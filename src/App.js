f4a17a15d3f695d3741fc7386fb2e17ebfcf7233
vartika <vartikably@gmail.com>
2024-11-17 21:14:17 +0530

    Changed title icon and title

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ function App() {
   const [word,setWord] = useState("");
   const [qrCode,setQrCode] = useState("");
   const [qrSize,setQrSize] = useState("100");
+  const [displayContent,setDisplayContent] = useState(false);
   useEffect(()=>{
     setQrCode("http://api.qrserver.com/v1/create-qr-code/?data="+word+"&size="+qrSize+"x"+qrSize);
     console.log(qrCode);
   },[word, qrSize]);
   const handleClick = ()=>{
     setWord(textInput);
+    setDisplayContent(true);
   }
   return (
     <div className="App">
@@ -23,14 +25,14 @@ function App() {
         <input type='text' className='bg-purple-50 border border-purple-300 text-purple-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-600 block  p-2.5 dark:bg-purple-700 dark:border-purple-600 dark:placeholder-purple-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500' value={textInput} onChange={(e)=>{setTextInput(e.target.value)}} placeholder='QR Generator Text'></input>
         <button  className='bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded' onClick={handleClick}>Generate</button>
       </div>
-      <div className="qr-code pt-8 mx-auto min-w-0">
+      {displayContent && (<div className="qr-code pt-8 mx-auto min-w-0">
         <input type='range' min="100" className="accent-purple-400 mx-auto" max="400" value={qrSize} onChange={(e)=>{setQrSize(e.target.value)}}></input>
         <img src={qrCode} className="pt-4 mx-auto" alt='QR Code'/>
         <br />
         <a href={qrCode} download="QR Code" target='_blank' rel="noreferrer">
           <button className='bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded'>Download</button>
-        </a>      
-      </div>
+        </a>    
+      </div>)}
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+const footer = ()=>{
+    return(
+        <div class="bg-gray-100 pt-2">
+            <div class="my-5">By Vartika Agarwal™ © Copyright 2024. All Rights Reserved.</div>
+        </div>
+    )
+}
+export default footer;
